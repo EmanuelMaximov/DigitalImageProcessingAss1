d749aa19cfd8965d6c175ae53d4ba3a391d952a8
Emanuel Maximov <maximov@post.bgu.ac.il>
2022-11-14 23:13:37 +0200

    inverting is working

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ parab_first_point, parab_second_point = (), ()
 parab_coeffs = (0, 0, 0)
 poly = None
 half_rect = None
+parab_pts = None
 
 
 # ------------------------------Cubic Interpolation------------------------------
@@ -87,57 +88,99 @@ def nearest_neighbor_interpolation(img, new_size):
 
 
 # ------------------------------Bilinear Interpolation------------------------------
-# def bilinear_pixel(image, y, x):
-#     height = image.shape[0]
-#     width = image.shape[1]
-#
-#     x1 = max(min(math.floor(x), width - 1), 0)
-#     y1 = max(min(math.floor(y), height - 1), 0)
-#     x2 = max(min(math.ceil(x), width - 1), 0)
-#     y2 = max(min(math.ceil(y), height - 1), 0)
-#
-#     a = float(image[y1, x1])
-#     b = float(image[y2, x1])
-#     c = float(image[y1, x2])
-#     d = float(image[y2, x2])
-#
-#     dx = x - x1
-#     dy = y - y1
-#
-#     new_pixel = a * (1 - dx) * (1 - dy)
-#     new_pixel += b * dy * (1 - dx)
-#     new_pixel += c * dx * (1 - dy)
-#     new_pixel += d * dx * dy
-#     return round(new_pixel)
-#
-#
-# def bilinear_interpolation(image, new_height, new_width):
-#     new_image = np.zeros((new_height, new_width),
-#                          image.dtype)  # new_image = [[0 for _ in range(new_width)] for _ in range(new_height)]
-#
-#     orig_height = image.shape[0]
-#     orig_width = image.shape[1]
-#
-#     # Compute center column and center row
-#     x_orig_center = (orig_width - 1) / 2
-#     y_orig_center = (orig_height - 1) / 2
-#
-#     # Compute center of resized image
-#     x_scaled_center = (new_width - 1) / 2
-#     y_scaled_center = (new_height - 1) / 2
-#
-#     # Compute the scale in both axes
-#     scale_x = orig_width / new_width;
-#     scale_y = orig_height / new_height;
-#
-#     for y in range(new_height):
-#         for x in range(new_width):
-#             x_ = (x - x_scaled_center) * scale_x + x_orig_center
-#             y_ = (y - y_scaled_center) * scale_y + y_orig_center
-#
-#             new_image[y, x] = bilinear_pixel(image, y_, x_)
-#
-#     return new_image
+
+def bilinear_my_way(original_img, new_h, new_w):
+    # get dimensions of original image
+    # if the image is GrayScale the is no shape[2], if COLOR and in RGB then shape[2]=3
+    old_h, old_w = original_img.shape[:2]
+    # create an array of the desired shape and initialize with black pixels
+    resized = np.zeros((new_h, new_w))
+    # Calculate width and height scaling factors
+    w_scale_factor = (old_w) / (new_w) if new_h != 0 else 0
+    h_scale_factor = (old_h) / (new_h) if new_w != 0 else 0
+
+    for i in range(new_h):
+        for j in range(new_w):
+            # map the coordinates back to the original image = (x,y)=(i,j)*scale
+            x = i * h_scale_factor
+            y = j * w_scale_factor
+            # calculate the coordinate values for 4 (2x2) surrounding pixels.
+            x_floor = math.floor(x)
+            y_floor = math.floor(y)
+            ##ensure that its value remains in the range (0 to old_h-1) and (0 to old_w-1)
+            x_ceil = min(old_h - 1, math.ceil(x))
+            y_ceil = min(old_w - 1, math.ceil(y))
+
+            if (x_ceil == x_floor) and (y_ceil == y_floor):
+                q = original_img[int(x), int(y)]
+            elif (x_ceil == x_floor):
+                q1 = original_img[int(x), int(y_floor)]
+                q2 = original_img[int(x), int(y_ceil)]
+                # relative distance of q1 from (x,y) +relative distance of q2 from (x,y)
+                q = (q1 * (y_ceil - y)) + (q2 * (y - y_floor))
+            elif (y_ceil == y_floor):
+                q1 = original_img[int(x_floor), int(y)]
+                q2 = original_img[int(x_ceil), int(y)]
+                # relative distance of q1 from (x,y) +relative distance of q2 from (x,y)
+                q = (q1 * (x_ceil - x)) + (q2 * (x - x_floor))
+            else:
+                v1 = original_img[x_floor, y_floor]
+                v2 = original_img[x_ceil, y_floor]
+                v3 = original_img[x_floor, y_ceil]
+                v4 = original_img[x_ceil, y_ceil]
+
+                q1 = v1 * (x_ceil - x) + v2 * (x - x_floor)
+                q2 = v3 * (x_ceil - x) + v4 * (x - x_floor)
+                q = q1 * (y_ceil - y) + q2 * (y - y_floor)
+
+            resized[i, j] = q
+    return resized.astype(np.uint8)
+
+
+def bl_resize(original_img, new_h, new_w):
+    # get dimensions of original image
+    old_h, old_w, c = original_img.shape
+    # create an array of the desired shape.
+    # We will fill-in the values later.
+    resized = np.zeros((new_h, new_w, c))
+    # Calculate horizontal and vertical scaling factor
+    w_scale_factor = (old_w) / (new_w) if new_h != 0 else 0
+    h_scale_factor = (old_h) / (new_h) if new_w != 0 else 0
+    for i in range(new_h):
+        for j in range(new_w):
+            # map the coordinates back to the original image
+            x = i * h_scale_factor
+            y = j * w_scale_factor
+            # calculate the coordinate values for 4 surrounding pixels.
+            x_floor = math.floor(x)
+            x_ceil = min(old_h - 1, math.ceil(x))
+            y_floor = math.floor(y)
+            y_ceil = min(old_w - 1, math.ceil(y))
+
+            if (x_ceil == x_floor) and (y_ceil == y_floor):
+                q = original_img[int(x), int(y), :]
+            elif (x_ceil == x_floor):
+                q1 = original_img[int(x), int(y_floor), :]
+                q2 = original_img[int(x), int(y_ceil), :]
+                q = q1 * (y_ceil - y) + q2 * (y - y_floor)
+            elif (y_ceil == y_floor):
+                q1 = original_img[int(x_floor), int(y), :]
+                q2 = original_img[int(x_ceil), int(y), :]
+                q = (q1 * (x_ceil - x)) + (q2 * (x - x_floor))
+            else:
+                v1 = original_img[x_floor, y_floor, :]
+                v2 = original_img[x_ceil, y_floor, :]
+                v3 = original_img[x_floor, y_ceil, :]
+                v4 = original_img[x_ceil, y_ceil, :]
+
+                q1 = v1 * (x_ceil - x) + v2 * (x - x_floor)
+                q2 = v3 * (x_ceil - x) + v4 * (x - x_floor)
+                q = q1 * (y_ceil - y) + q2 * (y - y_floor)
+
+            resized[i, j, :] = q
+    return resized.astype(np.uint8)
+
+
 def bilinear_pixel(image, y, x):
     height = image.shape[0]
     width = image.shape[1]
@@ -174,19 +217,14 @@ def bilinear_interpolation(image, new_height, new_width):
     y_orig_center = (orig_height - 1) / 2
 
     # Compute center of resized image
-
+    x_scaled_center = (new_width - 1) / 2
     y_scaled_center = (new_height - 1) / 2
 
     # Compute the scale in both axes
-
-    scale_y = orig_height / new_height
+    scale_x = orig_width / new_width;
+    scale_y = orig_height / new_height;
 
     for y in range(new_height):
-        width = poly(y)
-        x_scaled_center = (width - 1) / 2
-        scale_x = round(orig_width / width)
-
-        # maybe change new_width here:
         for x in range(new_width):
             x_ = (x - x_scaled_center) * scale_x + x_orig_center
             y_ = (y - y_scaled_center) * scale_y + y_orig_center
@@ -204,7 +242,8 @@ def resize(img, new_height, new_width, interpolation=None):
         return bilinear_interpolation(img, new_height, new_width)
 
     elif interpolation == 'cubic':
-        return cv2.resize(img, (new_height, new_width), interpolation=cv2.INTER_CUBIC)
+        # return cv2.resize(img, (new_height, new_width), interpolation=cv2.INTER_CUBIC)
+        return cubic_interpolation(img, (1.2, 1.2))
 
 
 def draw_rectangle():
@@ -224,7 +263,7 @@ def draw_rectangle():
 
 
 def draw_parabola(parab_clicked_point_x, parab_clicked_point_y):
-    global parab_point_button, parab_coeffs, poly
+    global parab_point_button, parab_coeffs, poly, parab_pts
     pts = np.array([[parab_first_point[0], parab_first_point[1]],
                     [parab_clicked_point_x, parab_clicked_point_y],
                     [parab_second_point[0], parab_second_point[1]]], np.int32)
@@ -293,9 +332,9 @@ def compute_cropped_area(vertex):
     #
     # Bilinear
 
-    rect = original_image[top_left_corner[0][1]:bottom_right_corner[0][1],
-           top_left_corner[0][0]:bottom_right_corner[0][0]]
-    rect = resize(rect, rect.shape[0], rect.shape[1], interpolation='bilinear')
+    # rect = original_image[top_left_corner[0][1]:bottom_right_corner[0][1],
+    #        top_left_corner[0][0]:bottom_right_corner[0][0]]
+    # rect = resize(rect, rect.shape[0], rect.shape[1], interpolation='bilinear')
 
     # resized_left_half_bilinear = resize(cropped_left_half, new_height, new_width_left, interpolation='bilinear')
     # resized_right_half_bilinear = resize(cropped_right_half, new_height, new_width_right, interpolation='bilinear')
@@ -306,9 +345,35 @@ def compute_cropped_area(vertex):
     # resized_right_half_BC = resize(cropped_right_half, new_height, new_width_right, interpolation='cubic')
     # new_image_BC = np.concatenate((resized_left_half_BC, resized_right_half_BC), axis=1)
 
-    cv2.imshow("Bilinear", rect)
+    # cv2.imshow("Bilinear", rect)
     # cv2.imshow("Nearest Neighbor", new_image_NN)
     # cv2.imshow("Cubic", resized_left_half_BC)
+
+
+def new_deformat(img):
+    col, row = img.shape[:2]
+    scaled_image = np.zeros((col, row), image.dtype)
+    rect_half_width = 0.5 * (bottom_right_corner[0][0] - top_left_corner[0][0])
+    rect_half_x = round((bottom_right_corner[0][0] + top_left_corner[0][0]) / 2)
+
+    for y in range(top_left_corner[0][1], bottom_right_corner[0][1]):
+        parab_x = poly(y)
+        relative_left_parab_x = parab_x - top_left_corner[0][0]
+        relative_right_parab_x = parab_x - rect_half_x
+        for x in range(top_left_corner[0][0], bottom_right_corner[0][0]):
+            if x <= rect_half_x:
+                relative_x = x - top_left_corner[0][0]
+                x_scale = relative_x / rect_half_width
+                delta = round(relative_left_parab_x * x_scale)
+                scaled_image[y - col, top_left_corner[0][0] + delta - row] = img[y, x]
+            else:
+                relative_x = (x - rect_half_x)
+                x_scale = 1 - (relative_x / rect_half_width)
+                #
+                delta = round((rect_half_width - relative_right_parab_x) * (x_scale))
+                scaled_image[y - col, bottom_right_corner[0][0] - delta - row] = img[y, x]
+    cv2.imshow("Assignment 2", scaled_image)
+    cv2.waitKey(0)
 
 
 def deformat(img):
@@ -327,14 +392,14 @@ def deformat(img):
         new_width_right = bottom_right_corner[0][0] - eq
         new_height = 1
 
-        cropped_left = resize(cropped_left, new_height, new_width_left, interpolation='nearest_neighbor')
-        cropped_right = resize(cropped_right, new_height, new_width_right, interpolation='nearest_neighbor')
-        print("wanted length: ", bottom_right_corner[0][0] - top_left_corner[0][0])
-        print("summed length: ", cropped_left.shape[1] + cropped_right.shape[1])
-        print("cropped left: ", "width:", cropped_left.shape[1], "height:", cropped_left.shape[0])
-        # print(cropped_left)
-        print("cropped right: ", "width:", cropped_right.shape[1], "height:", cropped_right.shape[0])
-        # print(cropped_right)
+        cropped_left = resize(cropped_left, new_height, new_width_left, interpolation='bilinear')
+        cropped_right = resize(cropped_right, new_height, new_width_right, interpolation='bilinear')
+        # print("wanted length: ", bottom_right_corner[0][0] - top_left_corner[0][0])
+        # print("summed length: ", cropped_left.shape[1] + cropped_right.shape[1])
+        # print("cropped left: ", "width:", cropped_left.shape[1], "height:", cropped_left.shape[0])
+        # # print(cropped_left)
+        # print("cropped right: ", "width:", cropped_right.shape[1], "height:", cropped_right.shape[0])
+        # # print(cropped_right)
         full = np.concatenate((cropped_left, cropped_right), axis=1)
         if full.shape[1] < bottom_right_corner[0][0] - top_left_corner[0][0]:
             full = np.concatenate((full, [[100]]), axis=1)
@@ -345,26 +410,22 @@ def deformat(img):
         else:
             concated_image = np.concatenate((concated_image, full), axis=0)
 
-    # left = img[0:img.shape[0],
-    #        0:  top_left_corner[0][0]]
-    # right = img[0:img.shape[0],
-    #         bottom_right_corner[0][0]:  img.shape[1]]
-    # top = img[0:top_left_corner[0][1],
-    #       top_left_corner[0][0]: bottom_right_corner[0][0]]
-    # bottom = img[bottom_right_corner[0][1]:img.shape[0],
-    #          top_left_corner[0][0]: bottom_right_corner[0][0]]
-    # middle = np.concatenate((top, concated_image), axis=0)
-    # middle = np.concatenate((middle, bottom), axis=0)
-    # print("left:", left.shape[0], left.shape[1])
-    # print("middle:", middle.shape[0], middle.shape[1])
-    # full = np.concatenate((left, middle), axis=1)
-    # full = np.concatenate((full, right), axis=1)
-    # print("new:", concated_image.shape[0], concated_image.shape[1])
-    # print("width", bottom_right_corner[0][0] - top_left_corner[0][0])
-    # # print("top:", top.shape[0], top.shape[1])
-    # # print("bottom:", bottom.shape[0], bottom.shape[1])
-
-    return concated_image
+    left = img[0:img.shape[0],
+           0:  top_left_corner[0][0]]
+    right = img[0:img.shape[0],
+            bottom_right_corner[0][0]:  img.shape[1]]
+    top = img[0:top_left_corner[0][1],
+          top_left_corner[0][0]: bottom_right_corner[0][0]]
+    bottom = img[bottom_right_corner[0][1]:img.shape[0],
+             top_left_corner[0][0]: bottom_right_corner[0][0]]
+    middle = np.concatenate((top, concated_image), axis=0)
+    middle = np.concatenate((middle, bottom), axis=0)
+    full = np.concatenate((left, middle), axis=1)
+    full = np.concatenate((full, right), axis=1)
+    cv2.polylines(full, [parab_pts], False, (255, 0, 0), 3)
+    cv2.imshow("new", full)
+    cv2.waitKey(0)
+    return full
 
 
 # function which will be called on mouse input
@@ -419,10 +480,14 @@ if __name__ == '__main__':
     if os.path.exists(relative_path):
         load_image()
         display_window()
-        # x = deformat(original_image)
-        # print(x)
-        # cv2.imshow("Assignment 1", x)
+        # x = bilinear_my_way(image, 500, 500)
+        # y = bilinear_interpolation(image, 500, 500)
+        # cv2.imshow("My way", x)
+        # cv2.imshow("other", y)
         # cv2.waitKey(0)
+        new_deformat(original_image)
+        # print(x)
+
         # x = cubic_interpolation(image, (1, 1.2))
 
     else:
